d832d4d87249d6a75e5107e448bdcc8a9cd5fabe
Praneeth Welideniya <mpraneethw@gmail.com>
2017-06-07 18:43:56 +0530

    create sign up controller

--- src/main/java/com/praneeth/spring/module/customer/controller/CustomerRestController.java
+++ src/main/java/com/praneeth/spring/module/customer/controller/CustomerRestController.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.praneeth.spring.module.customer.model.Customer;
+import com.praneeth.spring.module.customer.model.User;
 import com.praneeth.spring.module.customer.service.CustomerService;
 
 
 
 @RestController
-
+@RequestMapping(value="/")	
 public class CustomerRestController {
 	
 	@Autowired
@@ -34,6 +36,12 @@ public class CustomerRestController {
 		customerService.add(customer);
 	}
 	
+	@CrossOrigin
+	@PostMapping("/signup")
+	public User signUp(@RequestBody User user){
+		return user;
+		
+	}
 	@CrossOrigin
 	@GetMapping("/all")
 	public List<Customer> getAll(){
